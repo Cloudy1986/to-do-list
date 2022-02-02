64600412933d27473282668263deee2178888be6
Mark McLeod <markmcleod45@gmail.com>
2022-02-02 14:08:36 +0000

    Feature - view homepage

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class ToDoListManager < Sinatra::Base
   end
 
   get '/' do
-    "Hello World"
+    erb :homepage
   end
 
   run! if app_file == $0
---
+++ spec/features/user_can_view_homepage_spec.rb
@@ -0,0 +1,6 @@
+feature 'Homepage' do
+  scenario 'user can view homepage' do
+    visit '/'
+    expect(page).to have_content 'To Do List Manager'
+  end
+end
