c5c37e442fc721863c1f242832aad3cbaa1abd9f
Mark McLeod <markmcleod45@gmail.com>
2022-02-03 10:30:04 +0000

    Add feature test for deleting a task

---
+++ spec/features/user_can_delete_task_spec.rb
@@ -0,0 +1,13 @@
+feature '' do
+  scenario '' do
+    
+    visit '/to-do'
+    click_link 'Add Task'
+    fill_in 'title', with: 'Buy toiletries'
+    click_button 'Submit'
+    click_button 'Delete'
+    expect(page).to have_content 'To Do List'
+    expect(page).not_to have_content 'Buy toiletries'
+
+  end
+end
